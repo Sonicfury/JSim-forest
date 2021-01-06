ba5e7060e0d29393f4ff1e60a974b0d385232b22
jbloup <59645891+jbloup@users.noreply.github.com>
2021-01-06 17:11:18 +0100

    feat/add class cell (#4)
    * feat: add CellType & Cell
    
    * chore: removed blank lines
    
    * chore: update JavaDoc
    
    Co-authored-by: 2SCSob <49813786+Sonicfury@users.noreply.github.com>
    Co-authored-by: Qvandenhove <62334183+Qvandenhove@users.noreply.github.com>

---
+++ src/main/java/com/jsimforest/Cell.java
@@ -0,0 +1,94 @@
+package com.jsimforest;
+
+public class Cell {
+
+    private CellType cellType;
+    private int coordX;
+    private int coordY;
+    private Health health;
+
+    /**
+     * Configuration Cell - default
+     */
+
+    public Cell(){
+        this.cellType = new CellType();
+        this.coordX = 1;
+        this.coordY = 1;
+        this.health = Health.ok;
+    }
+
+    /**
+     * Overloading Cell Constructor - full
+     * @param cellType type of Cell
+     * @param coordX coordinate of cell on the axe X
+     * @param coordY coordinate of cell on the axe Y
+     * @param health state of Cell with enum
+     */
+
+    public Cell(CellType cellType, int coordX, int coordY, Health health){
+        this.cellType = cellType;
+        this.coordX = coordX;
+        this.coordY = coordY;
+        this.health = health;
+    }
+
+    public CellType getCellType() {
+        return cellType;
+    }
+
+    /**
+     *
+     * @param cellType type of Cell
+     */
+    public void setCellType(CellType cellType) {
+        this.cellType = cellType;
+    }
+
+    public int getCoordX() {
+        return coordX;
+    }
+
+    /**
+     *
+     * @param coordX coordinate of cell on the X axis
+     * @throws IllegalArgumentException if the coordinate X is inferior or equal to 0
+     */
+    public void setCoordX(int coordX) {
+        if(coordX >= 0){
+            this.coordX = coordX;
+        }
+        else{
+            throw new IllegalArgumentException("coordX must be superior or equal to 0");
+        }
+    }
+
+    public int getCoordY() {
+        return coordY;
+    }
+
+    /**
+     *
+     * @param coordY  coordinate of cell on the Y axis
+     */
+    public void setCoordY(int coordY) {
+        if(coordY >= 0) {
+            this.coordY = coordY;
+        }
+        else {
+            throw new IllegalArgumentException("coordY must be superior or equal to 0");
+        }
+    }
+
+    public Health getHealth() {
+        return health;
+    }
+
+    /**
+     *
+     * @param health state of cell with a enum
+     */
+    public void setHealth(Health health) {
+        this.health = health;
+    }
+}
---
+++ src/test/java/com/jsimforest/CellTypeTest.java
@@ -0,0 +1,23 @@
+package com.jsimforest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CellTypeTest extends AbstractTest {
+
+    @Test
+    public void setCellType(){
+       CellType cellType = new CellType();
+
+        String name = "tree";
+        String color = "green";
+
+        cellType.setName(name);
+        cellType.setColor(color);
+
+        assertEquals(name, cellType.getName());
+        assertEquals(color, cellType.getColor());
+    }
+
+}
